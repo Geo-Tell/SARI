97c4f2c004e08e5dec6bbe615df7b8f52fbf1cee
geovision <zhengxw104@163.com>
2023-06-11 10:38:20 +0800

    Create eval_gid_cv.sh

---
+++ SPSNet/eval_gid_cv.sh
@@ -0,0 +1,16 @@
+Path='./res/SPSNet_gid_cv'
+Root='xx/GID/data_1024'
+Backbone='Resnet101'
+pth='best'
+ModelFile='models.SPSNet_gid_feat'
+
+CUDA_VISIBLE_DEVICES=0,1 python -u evaluate_cv.py \
+--Root ${Root} \
+--Path ${Path} \
+--PthFile ${pth} \
+--Backbone ${Backbone} \
+--ModelFile ${ModelFile} \
+--Validset traval.lst \
+--dataset gid \
+--Crop \
+--MultiScale
