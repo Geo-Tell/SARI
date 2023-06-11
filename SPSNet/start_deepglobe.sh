dc9bb89f6278899907453bab31ae136f7751e4f3
geovision <zhengxw104@163.com>
2023-06-11 10:34:38 +0800

    Create start_deepglobe.sh

---
+++ SPSNet/start_deepglobe.sh
@@ -0,0 +1,27 @@
+Path='./res/SPSNet_deepglobe'
+Root='xx/DeepGlobe/data_768'
+Backbone='Resnet50'
+ModelFile='models.SPSNet_deepglobe_feat'
+Port='12345'
+CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nnodes=1 --nproc_per_node=2 --master_port ${Port} train_sp.py \
+--resPath ${Path} \
+--Root ${Root} \
+--Backbone ${Backbone} \
+--dataset deepglobe \
+--Trainset train.lst \
+--Validset val.lst \
+--Epoch 125 \
+--Delay 75 \
+--HFTrans \
+--VFTrans \
+--RRTrans \
+--lrStart1 3e-4 \
+--lrStart2 3e-4 \
+--lamda1 0.1 \
+--lamda2 0.1 \
+--lamda3 50 \
+--ModelFile ${ModelFile} \
+--BestNum 1 \
+--ClassWeight \
+--Background \
+# --Resume
